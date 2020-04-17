03dce4bd3dd9d7995958b29a378e0faba980487c
Ross <Rharper9@gmail.com>
2020-04-17 15:24:07 -0500

    added a few gems

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,8 +2,12 @@ class Article < ApplicationRecord
     has_many :comments
     has_many :taggings
     has_many :tags, through: :taggings
+    has_attached_file :image
+    validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png"]
     def tag_list
-        tags.join(", ")
+        self.tags.collect do |tag|
+            tag.name
+        end.join(", ")
     end
     def tag_list=(tags_string)
         tag_names = tags_string.split(",").collect{|s| s.strip.downcase}.uniq
